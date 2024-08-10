7f42190a729bbda23f9ebdacc83933869de8c440
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-08-10 09:26:48 +0300

    task 2 css done

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,13 @@
+import clsx from "clsx";
+import css from "./FriendListItem.module.css";
 export default function FriendListItem({ friend: { avatar, name, isOnline } }) {
   return (
-    <div>
+    <div className={css.friendCard}>
       <img src={avatar} alt="Avatar" width="48" />
-      <p>Friend name{name}</p>
-      <p>Friend status{isOnline}</p>
+      <p className={css.text}>{name}</p>
+      <p className={clsx(isOnline ? css.online : css.offline)}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
     </div>
   );
 }
